9d9d7bb895d8d8bd072525029367967bb5fe9de6
David <david.chihyun.park@gmail.com>
2019-03-05 21:09:04 +0900

    [#1][INITIAL] 개발환경 세팅
    개발 환경 세팅
    
    Resolves #1

---
+++ src/main/java/com/davidpark/board/HomeController.java
@@ -0,0 +1,39 @@
+package com.davidpark.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "generic";
+	}
+
+	@RequestMapping(value = "/list_view", method = RequestMethod.GET)
+	public String listView() {
+		return "generic";
+	}
+	
+	@RequestMapping(value = "/photo_preview", method = RequestMethod.GET)
+	public String photoPreview() {
+		return "generic";
+	}
+	
+	@RequestMapping(value = "/generic", method = RequestMethod.GET)
+	public String generic() {
+		return "generic";
+	}
+	
+	@RequestMapping(value = "/elements", method = RequestMethod.GET)
+	public String elements() {
+		return "elements";
+	}
+}
